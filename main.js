b988bdc1705dedf9da0019251bb41d099d2f7593
Braden Gerace <braden.gerace@hotmail.com>
2023-05-18 16:11:06 -0400

    2

--- main.js
+++ main.js
@@ -2,5 +2,11 @@ const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function() {
-    console.log(links.classList);
+    // console.log(links.classList.contains());
+    if (links.classList.contains("links")){
+        links.classList.remove("links");
+    }
+    else {
+        links.classList.add("links")
+    }
 })
